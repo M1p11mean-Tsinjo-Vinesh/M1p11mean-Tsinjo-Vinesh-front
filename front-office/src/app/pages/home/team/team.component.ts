b5479b476d8f7ff8b9a9ac571537eed7ce1e84cf
TsinjoAndrianarivony <tsinjo.andrianarivony@gmail.com>
2024-02-27 10:50:49 +0300

    [Profile] - images on employee fix + members into store

--- front-office/src/app/pages/home/team/team.component.ts
+++ front-office/src/app/pages/home/team/team.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {TeamMemberProps} from "../../../../components/common-components/team-member-card/team-member-card.component";
-import {TeamMemberService} from "../../../services/team-member/team-member.service";
+import {Store} from "@ngrx/store";
+import AppStore from "../../../store/Appstore";
 
 @Component({
   selector: 'app-team',
@@ -11,21 +12,12 @@ export class TeamComponent implements OnInit {
 
   members: TeamMemberProps[] = [];
 
-  constructor(private service: TeamMemberService) {
+  constructor(private store: Store<AppStore>) {
   }
 
   ngOnInit() {
-    this.service.findTeamMembers().subscribe(list => {
-      this.members = list.map(member => {
-        //TODO: update theses temporary photos when employees have photos
-        member.image = [
-          "https://res.cloudinary.com/dje2mveih/image/upload/v1708810354/assets/gallery-7_urlwlc.jpg",
-          "https://res.cloudinary.com/dje2mveih/image/upload/v1708810354/assets/image-gallery-02_nsmzdx.jpg",
-          "https://res.cloudinary.com/dje2mveih/image/upload/v1708810354/assets/image-gallery-03_kxlerz.jpg",
-          "https://res.cloudinary.com/dje2mveih/image/upload/v1708810354/assets/image-gallery-01_wkbcky.jpg"
-        ].at(Math.floor(Math.random() * 4));
-        return member;
-      });
+    this.store.subscribe(({members}) => {
+      this.members = members.list;
     })
   }
 
