905106c6cf9c2476353bda6a9ef8415ce3e647da
TsinjoAndrianarivony <tsinjo.andrianarivony@gmail.com>
2024-02-27 11:22:59 +0300

    [Home][About-us] - dynamic count on about-us

--- front-office/src/app/pages/home/about-us/about-us.component.ts
+++ front-office/src/app/pages/home/about-us/about-us.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Store} from "@ngrx/store";
+import AppStore from "../../../store/Appstore";
 
 @Component({
   selector: 'app-about-us',
   templateUrl: './about-us.component.html',
   styleUrls: ['./about-us.component.css']
 })
-export class AboutUsComponent {
+export class AboutUsComponent implements OnInit {
+
+  count = 0;
+
+  constructor(private store:Store<AppStore>) {
+  }
+
+
+  ngOnInit() {
+    this.store.subscribe(({members}) => {
+      this.count = members.list.length;
+    })
+  }
+
 
 }
